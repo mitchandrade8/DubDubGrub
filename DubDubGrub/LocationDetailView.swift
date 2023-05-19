78a4f4814af36863f0ce0d6817fc75b3c1d49e01
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-05-19 13:25:43 -0600

    LocationDetailView Start -- taking break

---
+++ DubDubGrub/LocationDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  LocationDetailView.swift
+//  DubDubGrub
+//
+//  Created by Mitch Andrade on 5/19/23.
+//
+
+import SwiftUI
+
+struct LocationDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LocationDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationDetailView()
+    }
+}
