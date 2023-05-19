37967fb89847cd94855f0d2e7f9228a1972adcbc
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-05-19 10:37:06 -0600

    Initial Commit

---
+++ DubDubGrub/DubDubGrubApp.swift
@@ -0,0 +1,17 @@
+//
+//  DubDubGrubApp.swift
+//  DubDubGrub
+//
+//  Created by Mitch Andrade on 5/19/23.
+//
+
+import SwiftUI
+
+@main
+struct DubDubGrubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
