65a8f51c2153de96dc4fbbbaee3bb255fb5ec611
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-05-20 22:45:50 -0600

    LocationDetail View -- end

--- DubDubGrub/LocationDetailView.swift
+++ DubDubGrub/LocationDetailView.swift
@@ -8,6 +8,12 @@
 import SwiftUI
 
 struct LocationDetailView: View {
+    
+    let columns = [GridItem(.flexible()),
+                   GridItem(.flexible()),
+                   GridItem(.flexible())
+    ]
+    
     var body: some View {
         NavigationView {
             VStack(spacing: 16) {
@@ -30,6 +36,47 @@ struct LocationDetailView: View {
                     .minimumScaleFactor(0.75)
                     .padding(.horizontal)
                 
+                ZStack {
+                    Capsule()
+                        .frame(height: 80)
+                        .foregroundColor(Color(uiColor: .secondarySystemBackground))
+                    
+                    HStack(spacing: 20) {
+                        Button {
+                            
+                        } label: {
+                            LocationActionButton(color: .brandPrimary, imageName: "location.fill")
+                        }
+                        
+                        Link(destination: URL(string: "https://aaple.com")!, label: {
+                            LocationActionButton(color: .brandPrimary, imageName: "network")
+                        })
+                       
+                        Button {
+                            
+                        } label: {
+                            LocationActionButton(color: .brandPrimary, imageName: "phone.fill")
+                        }
+                        
+                        Button {
+                            
+                        } label: {
+                            LocationActionButton(color: .brandPrimary, imageName: "person.fill.checkmark")
+                        }
+                    }
+                }
+                .padding(.horizontal)
+                
+                Text("Who's Here?")
+                    .bold()
+                    .font(.title2)
+                
+                LazyVGrid(columns: columns, content: {
+                    FirstNameAvatarView(firstName: "Lebron")
+                    FirstNameAvatarView(firstName: "Kyrie")
+                    FirstNameAvatarView(firstName: "Kawhi")
+                })
+                
                 Spacer()
             }
             .navigationTitle("Location Name")
@@ -41,5 +88,41 @@ struct LocationDetailView: View {
 struct LocationDetailView_Previews: PreviewProvider {
     static var previews: some View {
         LocationDetailView()
+    }
+}
+
+struct LocationActionButton: View {
+    
+    var color: Color
+    var imageName: String
+    
+    var body: some View {
+        ZStack {
+            Circle()
+                .foregroundColor(color)
+                .frame(width: 60, height: 60)
+            
+            Image(systemName: imageName)
+                .resizable()
+                .scaledToFit()
+                .foregroundColor(.white)
+                .frame(width: 22, height: 22)
+        }
+    }
+}
+
+struct FirstNameAvatarView: View {
+    
+    var firstName: String
+    
+    var body: some View {
+        VStack {
+            AvatarView(size: 64)
+            
+            Text(firstName)
+                .bold()
+                .lineLimit(1)
+                .minimumScaleFactor(0.1)
+        }
     }
 }
