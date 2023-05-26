ed0c00ab821f13e8dc78409c3060abd37e341ee7
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-05-26 10:02:13 -0600

    -- finish

--- DubDubGrub/Utilities/Extensions/UIImage+Ext.swift
+++ DubDubGrub/Utilities/Extensions/UIImage+Ext.swift
@@ -5,8 +5,9 @@
 //  Created by Mitch Andrade on 5/26/23.
 //
 
-import ClockKit
+import CloudKit
 import UIKit
+
 
 extension UIImage {
     
@@ -18,13 +19,18 @@ extension UIImage {
         }
         
         // 2. Append some unique identifier for our profile image
-        
+        let fileUrl = urlPath.appendingPathComponent("selectedAvatarImage")
         
         // 3. Write the image data to the location the address points to
-        
+        guard let imageData = jpegData(compressionQuality: 0.25) else { return nil }
         
         // 4. Create our CKAsset with that fileURL
-        
+        do {
+            try imageData.write(to: fileUrl)
+            return CKAsset(fileURL: fileUrl)
+        } catch {
+            return nil
+        }
         
         return nil
     }
