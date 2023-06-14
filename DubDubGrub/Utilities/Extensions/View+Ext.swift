2a8222f72616b20c2556a94b4bbea940127021b7
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-06-14 05:31:29 -0600

    -- Finished - Haptic Feedback

--- DubDubGrub/Utilities/Extensions/View+Ext.swift
+++ DubDubGrub/Utilities/Extensions/View+Ext.swift
@@ -8,11 +8,18 @@
 import SwiftUI
 
 extension View {
+    
     func profileNameStyle() -> some View {
         self.modifier(ProfileNameText())
+    }
+    
+    func playHaptic() {
+        let generator = UINotificationFeedbackGenerator()
+        generator.notificationOccurred(.success)
     }
     
     func dismissKeyboard() {
         UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
     }
+    
 }
